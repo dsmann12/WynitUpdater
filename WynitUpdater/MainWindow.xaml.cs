af5834a9952c112d470502ee1ee7b420a40a6329
David Scheuermann <dsmann1212@gmail.com>
2017-09-18 22:07:40 -0500

    First commit

---
+++ WynitUpdater/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace WynitUpdater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string filename;
+        public MainWindow()
+        {
+            WynitUpdate.LoadSkus();
+            InitializeComponent();
+            this.textBox.Text = Directory.GetCurrentDirectory();
+            this.textBox.TextWrapping = TextWrapping.NoWrap;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            d.ShowDialog();
+            filename = d.FileName;
+            this.textBox.Text = filename;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if(filename == null)
+            {
+                MessageBox.Show("No file was selected!");
+            }
+            else
+            {
+                try
+                {
+                    WynitUpdate.ReadWynitFile(filename);
+                    WynitUpdate.GenerateFile();
+                    MessageBox.Show("Done!"); 
+                }
+                catch(IndexOutOfRangeException ex)
+                {
+                    MessageBox.Show("A problem occurred. Make sure the correct file was selected");
+                }
+                catch(ArgumentException ex)
+                {
+                    MessageBox.Show("A porblem occurred. Make sure the correct file was selected ");
+                }
+            }
+        }
+    }
+}
---
+++ WynitUpdater/WynitUpdate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WynitUpdater
+{
+    static class WynitUpdate
+    {
+        private static List<string> skus = new List<string>();
+        private static Dictionary<string, string> wynit = new Dictionary<string, string>();
+
+        public static void LoadSkus()
+        {
+            try
+            {
+                StreamReader f = new StreamReader("wynitskus.csv");
+                while (!f.EndOfStream)
+                {
+                    string line = f.ReadLine();
+                    skus.Add(line);
+                }
+                Console.WriteLine(skus[0]);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void ReadWynitFile(string filename)
+        {
+            try
+            {
+                StreamReader f = new StreamReader(filename);
+                while (!f.EndOfStream)
+                {
+                    string[] row = f.ReadLine().Split('\t');
+                    wynit.Add(row[0].Substring(1, row[0].Length - 2), row[5]);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+                wynit.Clear();
+            }
+            catch(ArgumentException ex)
+            {
+                wynit.Clear();
+                ReadWynitFile(filename);
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static void GenerateFile()
+        {
+            using (StreamWriter w = File.CreateText("WynitQty.csv"))
+            {
+                foreach (string sku in skus)
+                {
+                    try
+                    {
+                        string value = wynit[sku.Substring(4)];
+                        w.WriteLine("{0}, {1}", sku, value);
+                    }
+                    catch (KeyNotFoundException ex)
+                    {
+                        w.WriteLine("{0}, 0", sku);
+                    }
+                }
+            }
+        }
+    }
+}
